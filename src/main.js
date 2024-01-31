3e8f7a7a64ffc16da9b53450263bf7cbf1a66be1
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-01-31 13:19:25 +0100

    reset

--- src/main.js
+++ src/main.js
@@ -71,6 +71,7 @@ async function onSubmit(e) {
 
     listEl.innerHTML = markUp(arr);
     modal.refresh();
+    serchQuery = '';
     loader.classList.add('is-hidden');
   } catch (error) {
     iziToast.error({
@@ -80,6 +81,8 @@ async function onSubmit(e) {
         'Sorry, there are no images matching your search query. Please try again!',
     });
   }
+  btnLoadMore.classList.remove('is-hidden');
+  formEl.reset();
 }
 
 function markUp(arr) {
